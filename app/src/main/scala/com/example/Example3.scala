cb6d3cded43aebbcb3dde990a59f4df5e5ab7d6e
sutha <!#bin/bash/git/88>
2023-07-16 15:31:51 +0530

    Apache Spark with mongodb

--- app/src/main/scala/com/example/Example3.scala
+++ app/src/main/scala/com/example/Example3.scala
@@ -1,6 +1,7 @@
 package com.example
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.Dataset
 
 object Example3 {
 
@@ -8,24 +9,32 @@ object Example3 {
 
     var spark = SparkSession
       .builder
-      .appName("chapter2")
+      .appName("Spark MongoDB")
       .master("local[*]")
       .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.13:10.2.0")
-
+      .config("spark.mongodb.read.connection.uri", "mongodb://localhost:27017/latest_db.students")
+      .config("spark.mongodb.write.connection.uri", "mongodb://localhost:27017/latest_db.students")
       .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    val mongodf = spark.read.format("mongodb")
-      .option("uri", "mongodb://localhost:27017")
-      .option("database", "latest_db")
-      .option("collection", "students")
-      .load()
+    val mongodf: DataFrame = spark.read.format("mongodb").load()
 
     mongodf.printSchema()
     mongodf.show()
-    mongodf.createOrReplaceTempView("tempMongo")
-    spark.sql("SELECT * FROM tempMongo").show()
+    //mongodf.createOrReplaceTempView("tempMongo")
+    //spark.sql("SELECT * FROM tempMongo").show()
+
+
+    //write data from json to mongodb
+    val df = spark.read.format("json").load("app/src/main/resources/example.json")
+    df.write.format("mongodb").mode("overwrite").save()
+
+    //read data
+    val mongodf2: DataFrame = spark.read.format("mongodb").load()
+    mongodf2.show()
+
+
 
     spark.stop()
     println("completed")
